17687f916823b6c2e8fed45dd70dd19057eb39d9
Константин Короленко <konstantin.korolenko@infourok.biz>
2024-08-17 22:52:50 +0300

    access control

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[AsController]
+class AdminController extends Controller
+{
+    #[Route(path: '/admin', name: 'admin', methods: ['GET'])]
+    public function dashboard(): JsonResponse
+    {
+        $a = 1;
+        $b = 2;
+
+        return new JsonResponse([]);
+    }
+}
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -14,9 +14,10 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 
-class LoginFormAuthenticator extends AbstractAuthenticator
+class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
 {
     public function __construct(
         private readonly RouterInterface $router,
@@ -59,14 +60,10 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         );
     }
 
-    //    public function start(Request $request, AuthenticationException $authException = null): Response
-    //    {
-    //        /*
-    //         * If you would like this class to control what happens when an anonymous user accesses a
-    //         * protected page (e.g. redirect to /login), uncomment this method and make this class
-    //         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
-    //         *
-    //         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-    //         */
-    //    }
+        public function start(Request $request, AuthenticationException $authException = null): Response
+        {
+            return new RedirectResponse(
+                $this->router->generate('app_login')
+            );
+        }
 }
